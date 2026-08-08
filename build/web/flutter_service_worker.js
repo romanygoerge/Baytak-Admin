42aacea0b1e4204d26f39add573436a981f2df90
Baytak Admin <baytak.admin@example.com>
2026-08-08 11:26:33 +0300

    Add aggressive no-cache HTTP headers in vercel.json to force fresh JS loading

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "05eda506ba9578281158f6fe5e386834",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "43c2c5ffa2e33ea21cd1df01a89ad6b2",
+"flutter_bootstrap.js": "ec00285fa5574a3a3aa7ceddfa9230a9",
 "icons/Icon-192.png": "05eda506ba9578281158f6fe5e386834",
 "icons/Icon-512.png": "05eda506ba9578281158f6fe5e386834",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
