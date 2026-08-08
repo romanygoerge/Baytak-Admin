b404d54727bd8fafff0c5a725fdd729ada467478
Baytak Admin <baytak.admin@example.com>
2026-08-08 17:06:08 +0300

    Update admin dashboard web build with latest user management fixes and real-time updates

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "05eda506ba9578281158f6fe5e386834",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9da68138754abb255156bd5e7f8c87d2",
+"flutter_bootstrap.js": "b48f1a94b1e35c05926689b299903ae5",
 "icons/Icon-192.png": "05eda506ba9578281158f6fe5e386834",
 "icons/Icon-512.png": "05eda506ba9578281158f6fe5e386834",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
--- lib/features/admin/presentation/widgets/users_management.dart
+++ lib/features/admin/presentation/widgets/users_management.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:uuid/uuid.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/services/notification_service.dart';
 import '../providers/admin_providers.dart';
